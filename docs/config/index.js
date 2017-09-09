0a2713eced0772a0e496ba212b6d5cd71e3453c4
Roozbeh Hajizadeh <hajizadeh.roozbeh@gmail.com>
2017-09-10 01:54:56 +0430

    path updated

--- docs/config/index.js
+++ docs/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '//dyonir.github.io/vue-tinymce-editor/dist',
+    assetsPublicPath: '//dyonir.github.io/vue-tinymce-editor/dist/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
